7d9ec953f61931d3d18b4a582f92d0bc683080a0
Shubham Sharma <ss8680919@gmail.com>
2023-07-13 18:41:36 +0530

    updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 async function main(){
-    await mongoose.connect('mongodb+srv://placement:1UbhMXi13RXuCwSn@cluster0.p5gbb.mongodb.net/placementcell?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://ss8680919:ShWw7Mej1PfCsZVd@cluster0.sco9ldr.mongodb.net/?retryWrites=true&w=majority');
     console.log("connection Successfull !! ");
 }
 main().catch(error =>console.log("connection not successfull !!"));
